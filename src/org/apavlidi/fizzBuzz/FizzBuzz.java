77a079ff52d3c6f1cd38062d579e78dd98a22280
apavlidi <apavlidi@it.teithe.gr>
2019-10-22 20:30:29 +0300

    refactor fizzBuzz production code

--- src/org/apavlidi/fizzBuzz/FizzBuzz.java
+++ src/org/apavlidi/fizzBuzz/FizzBuzz.java
@@ -2,22 +2,25 @@ package org.apavlidi.fizzBuzz;
 
 public class FizzBuzz {
 
-    public static final String FIZZ_BUZZ = "FizzBuzz";
-    public static final String FIZZ = "Fizz";
-    public static final String BUZZ = "Buzz";
+  public static final String FIZZ_BUZZ = "FizzBuzz";
+  public static final String FIZZ = "Fizz";
+  public static final String BUZZ = "Buzz";
 
-    public static String printNumber(int number) {
-        if (isDivisibleBy(number, 3) && isDivisibleBy(number, 5)) {
-            return FIZZ_BUZZ;
-        } else if (isDivisibleBy(number, 3)) {
-            return FIZZ;
-        } else if (isDivisibleBy(number, 5)) {
-            return BUZZ;
-        }
-        return Integer.toString(number);
+  public static String printNumber(int number) {
+    if (isDivisibleBy(number, 3) && isDivisibleBy(number, 5)) {
+      return FIZZ_BUZZ;
+    }
+    if (isDivisibleBy(number, 3)) {
+      return FIZZ;
+    }
+    if (isDivisibleBy(number, 5)) {
+      return BUZZ;
     }
 
-    private static boolean isDivisibleBy(int dividend, int divisor) {
-        return dividend % divisor == 0;
-    }
+    return Integer.toString(number);
+  }
+
+  private static boolean isDivisibleBy(int dividend, int divisor) {
+    return dividend % divisor == 0;
+  }
 }
