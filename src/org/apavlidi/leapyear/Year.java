99519cb49a4cc4590b082e0dad672df9dc65f9ee
apavlidi <apavlidi@it.teithe.gr>
2019-11-29 00:45:33 +0200

    format code

--- src/org/apavlidi/leapyear/Year.java
+++ src/org/apavlidi/leapyear/Year.java
@@ -2,21 +2,21 @@ package org.apavlidi.leapyear;
 
 public class Year {
 
-    private int year;
+  private int year;
 
-    public Year(int year) {
-        this.year = year;
-    }
+  public Year(int year) {
+    this.year = year;
+  }
 
-    public boolean isLeapYear() {
-        return isDividedBy(4) && !(isDividedBy(100) && isNotDividedBy(400));
-    }
+  public boolean isLeapYear() {
+    return isDividedBy(4) && !(isDividedBy(100) && isNotDividedBy(400));
+  }
 
-    private boolean isNotDividedBy(int number) {
-        return year % number != 0;
-    }
+  private boolean isNotDividedBy(int number) {
+    return year % number != 0;
+  }
 
-    private boolean isDividedBy(int number) {
-        return year % number == 0;
-    }
+  private boolean isDividedBy(int number) {
+    return year % number == 0;
+  }
 }
